1319514d678129787b00801643020e1bbc2a3f08
ggdaltoso <ggdaltoso@gmail.com>
2026-04-23 09:30:09 -0300

    feat: remove Clippy CDN reference and clean up main.tsx
    This commit deletes the Clippy CDN reference from the main.tsx file to streamline the code and avoid unnecessary dependencies.

--- src/main.tsx
+++ src/main.tsx
@@ -10,10 +10,6 @@ import '../css/index.css';
 import './index.css';
 import { ModalProvider } from './ModalProvider.tsx';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-(window as any).CLIPPY_CDN =
-  'https://cdn.jsdelivr.net/gh/pi0/clippyjs/assets/agents/';
-
 const availableAgents = Object.keys(AGENTS);
 const randomIndex = Math.ceil(Math.random() * availableAgents.length);
 const agentIndex = availableAgents[randomIndex] as keyof typeof AGENTS;
